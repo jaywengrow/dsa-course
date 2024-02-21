ef91b767cd07fe9774b094ac73717fe52e3219b0
Jay Wengrow <jaywngrw@gmail.com>
2024-02-21 15:03:12 -0600

    preparing for week 5

---
+++ week_5/bst/same_tree.js
@@ -0,0 +1,8 @@
+function sameTree(firstNode, secondNode) {
+    if (!firstNode && !secondNode) { return true; }
+    if ((firstNode && !secondNode) || (!firstNode && secondNode)) { return false;}
+
+    return (firstNode.value === secondNode.value && sameTree(firstNode.leftChild, secondNode.leftChild) && sameTree(firstNode.rightChild, secondNode.rightChild));
+}
+
+export default sameTree;
